7e321dd686170175ae24e0561469368e3be1300b
Darren <darren@odden.io>
2019-01-03 12:17:28 -0800

    had to remove function_exists

--- simpleview.php
+++ simpleview.php
@@ -21,179 +21,163 @@ use PHPUnit\Runner\Exception;
 require_once __DIR__ . '/simpleViewLibrary.php';
 require_once __DIR__ . '/simpleViewFilter.php';
 
-if (function_exists('create') === false) {
-    /**
-     * Create the soap connection
-     *
-     * @return object SimpleView SOAP connection
-     */
-    function create()
-    {
-        $config = getConfig();
-        
-        return new SimpleViewLibrary($config);
-    }
+/**
+ * Create the soap connection
+ *
+ * @return object SimpleView SOAP connection
+ */
+function create()
+{
+    $config = getConfig();
+    
+    return new SimpleViewLibrary($config);
 }
 
-if (function_exists('filterAll') === false) {
-    /**
-     * Undocumented function
-     *
-     * @return object all listings filter
-     */
-    function filterAll()
-    {
-        $simpleViewFilter = new SimpleViewFilter(
-            $config->filter->fieldCategory,
-            $config->filter->fieldName,
-            $config->filter->filterType,
-            $config->filter->filterValue
-        );
+/**
+ * Undocumented function
+ *
+ * @return object all listings filter
+ */
+function filterAll()
+{
+    $simpleViewFilter = new SimpleViewFilter(
+        $config->filter->fieldCategory,
+        $config->filter->fieldName,
+        $config->filter->filterType,
+        $config->filter->filterValue
+    );
 
-        $allListings = $simpleViewFilter->filterAllListings();
+    $allListings = $simpleViewFilter->filterAllListings();
 
-        return $allListings;
-    }
+    return $allListings;
 }
 
-if (function_exists('svGetListings')) {
-    /**
-     * Undocumented function
-     *
-     * @param object  $connect           SOAP Connection
-     * @param array   $filterAllListings filter array for all listings
-     * @param integer $pageSize          page size, maximum 100
-     * @param integer $pageNumber        current page
-     * @param bool    $showAmenities     show ammenities; true or false
-     *
-     * @return object Listings
-     */
-    function svGetListings(
-        $connect,
-        $filterAllListings,
+/**
+ * Undocumented function
+ *
+ * @param object  $connection        SOAP Connection
+ * @param array   $filterAllListings filter array for all listings
+ * @param integer $pageSize          page size, maximum 100
+ * @param integer $pageNumber        current page
+ * @param bool    $showAmenities     show ammenities; true or false
+ *
+ * @return object Listings
+ */
+function getListings(
+    $connection,
+    $filterAllListings,
+    $pageSize,
+    $pageNumber,
+    $showAmenities
+) {
+    return $connection->getListings(
         $pageSize,
         $pageNumber,
+        $filterAllListings,
         $showAmenities
-    ) {
-        return $connect->getListings(
-            $pageSize,
-            $pageNumber,
-            $filterAllListings,
-            $showAmenities
-        );
-    }
+    );
 }
 
-if (function_exists('getResultsNumber')) {
-    /**
-     * Get the number of results to process
-     *
-     * @param object $connect       SOAP connection
-     * @param array  $filter        Array of filterAllListings
-     * @param bool   $showAmenities True or False
-     *
-     * @return integer number of results
-     */
-    function getResultsNumber(
-        $connect,
+/**
+ * Get the number of results to process
+ *
+ * @param object $connection    SOAP connection
+ * @param array  $filter        Array of filterAllListings
+ * @param bool   $showAmenities True or False
+ *
+ * @return integer number of results
+ */
+function getResultsNumber(
+    $connection,
+    $filter,
+    $showAmenities
+) {
+    $initial = $connection->getListings(
+        1,
+        1,
         $filter,
         $showAmenities
-    ) {
-        $initial = $connect->getListings(
-            1,
-            1,
-            $filter,
-            $showAmenities
-        );
+    );
 
-        return $initial['STATUS']['RESULTS'];
-    }
+    return $initial['STATUS']['RESULTS'];
 }
 
-if (function_exists('getListing') === false) {
-    /**
-     * Get listing from SimpleView
-     *
-     * @param object  $connect    SimpleView SOAP connection
-     * @param integer $listId     Listing ID
-     * @param integer $updateHits 0
-     *
-     * @return object listing
-     */
-    function getListing(
-        $connect,
-        $listId,
-        $updateHits
-    ) {
-        return $connect->getListing($listId, $updateHits);
-    }
+/**
+ * Get listing from SimpleView
+ *
+ * @param object  $connection SimpleView SOAP connection
+ * @param integer $listId     Listing ID
+ * @param integer $updateHits 0
+ *
+ * @return object listing
+ */
+function getListing(
+    $connection,
+    $listId,
+    $updateHits
+) {
+    return $connection->getListing($listId, $updateHits);
 }
 
-if (function_exists('getListingIds') === false) {
-    /**
-     * Undocumented function
-     *
-     * @param [type] $connect
-     * @param [type] $filter
-     * @param [type] $pageSize
-     *
-     * @return void
-     */
-    function getListingIds($connect, $filter, $pageSize)
-    {
-        $resultNumber = getResultsNumber($connect, $filter, 0);
+/**
+ * Undocumented function
+ *
+ * @param [type] $connection simpleview connection object
+ * @param [type] $filter     filter object
+ * @param [type] $pageSize   how many results per page to return
+ *
+ * @return void
+ */
+function getListingIds($connection, $filter, $pageSize)
+{
+    $resultNumber = getResultsNumber($connection, $filter, 0);
 
-        $pages = $resultNumber % $pageSize;
-        $results = array();
+    $pages = $resultNumber % $pageSize;
+    $results = array();
 
-        for ($page = 1; $page<=$pages; $page) {
-            $listings = $connect->getListings($pageSize, $page, $filter, 0);
-        
-            foreach ($listings["DATA"] as $listing) {
-                array_push($results, $listing["LISTINGID"]);
-            }
-        }
-        return $results;
-    }
-}
-
-if (function_exists('getConfig') === false) {
-    /**
-     * Undocumented function
-     *
-     * @return object
-     */
-    function getConfig()
-    {
-        $jsonStr = file_get_contents(__DIR__ . "/simpleview.json");
-        return json_decode($jsonStr);
-    }
-}
-
-if (function_exists('processListings') === false) {
-    /**
-     * Undocumented function
-     *
-     * @return void
-     */
-    function processListings()
-    {
-        $connection = create();
-        $filter     = filterAll();
-
-        $config = getConfig();
-        $pageSize = $config->pageSize;
-
-        $listingIds = getListingIds($connection, $filter, $pageSize);
-
-        foreach ($listingIds as $listingId) {
-            $listing = getListing($connection, $listingId, 0);
-
-            echo(var_dump($listing));
+    for ($page = 1; $page<=$pages; $page++) {
+        $listings = $connection->getListings($pageSize, $page, $filter, 0);
+    
+        foreach ($listings["DATA"] as $listing) {
+            array_push($results, $listing["LISTINGID"]);
         }
     }
+    return $results;
 }
 
+/**
+ * Undocumented function
+ *
+ * @return object
+ */
+function getConfig()
+{
+    $jsonStr = file_get_contents(__DIR__ . "/simpleview.json");
+    return json_decode($jsonStr);
+}
+
+/**
+ * Undocumented function
+ *
+ * @return void
+ */
+function processListings()
+{
+    $connection = create();
+    $filter     = filterAll();
+
+    $config = getConfig();
+    $pageSize = $config->pageSize;
+
+    $listingIds = getListingIds($connection, $filter, $pageSize);
+
+    foreach ($listingIds as $listingId) {
+        $listing = getListing($connection, $listingId, 0);
+    }
+}
+
+
+/////////////////////////////////
 $config = getConfig();
 
 $connect = create();
@@ -202,8 +186,8 @@ print var_dump($connect);
 $filter = filterAll();
 print var_dump($filter);
 
-$listings = $connect->getListings(1, 1, $filter, false);
-print var_dump($listings);
+// $listings = $connect->getListings(1, 1, $filter, false);
+// print var_dump($listings);
 
 $listingIds = getListingIds($connect, $filter, 100);
 print var_dump($listingIds);
